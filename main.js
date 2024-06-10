d91085ca4b55293319d12eb0781b0fda4d303ef3
Aryan Aman <70793631+aryanamann@users.noreply.github.com>
2024-06-10 16:07:21 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,27 @@
 function openNav() {
   document.getElementById("mySidenav").style.width = "250px";
   document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("hamburger").style.display = "none";
 }
 
 function closeNav() {
   document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("hamburger").style.display = "block";
   document.getElementById("main").style.marginLeft = "0";
 }
+
+let modal = document.getElementById("myModal");
+let span = document.getElementsByClassName("close")[0];
+
+span.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 let form = document.getElementById("form");
 let textInput = document.getElementById("textinput");
@@ -15,6 +30,8 @@ let textarea = document.getElementById("textarea");
 let tasks = document.getElementById("tasks");
 let navTasks = document.getElementById("navTasks");
 let add = document.getElementById("add");
+let date = document.getElementById("date");
+let detailsText = document.getElementById("detailInput");
 
 form.addEventListener("submit", (e) => {
   e.preventDefault();
@@ -28,12 +45,6 @@ let formValidation = () => {
   } else {
     console.log("success");
     acceptData();
-    add.setAttribute("data-bs-dismiss", "modal");
-    add.click();
-    console.log("object");
-    (() => {
-      add.setAttribute("data-bs-dismiss", "");
-    })();
   }
 };
 
@@ -42,6 +53,8 @@ let acceptData = () => {
   data.push({
     text: textInput.value,
     desc: textarea.value,
+    date: date.value,
+    details: detailsText.value,
   });
 
   localStorage.setItem("data", JSON.stringify(data));
@@ -54,13 +67,14 @@ let acceptData = () => {
 let createTasks = () => {
   tasks.innerHTML = "";
   data.map((x, y) => {
-    return (tasks.innerHTML += `<div id=${y}>
+    return (tasks.innerHTML += `<div id=${y} class="box">
     <p>${x.text}</p> 
-    <span class="mb-2">${x.desc}</span>
+    <p class="mb-2 fw-semibold">${x.desc}</p>
+    <p class="mb-2 fw-semibold">${x.date}</p>
     <span
-      ><i onclick="editTask(this)" data-bs-toggle="modal" data-bs-target="#form" class="fa-solid fa-pen-to-square"></i>
+      ><i onclick="editTask(this)"  class="fa-solid fa-pen-to-square"></i>
       <i onclick="deleteTask(this)" class="fa-solid fa-trash"></i
-    ></span>
+    ><i onclick="flagTask(this)" class="fa-solid fa-flag-checkered"></i></span>
   </div>`);
   });
 };
@@ -69,14 +83,24 @@ let createTasksNav = () => {
   navTasks.innerHTML = "";
   data.map((x, y) => {
     return (navTasks.innerHTML += `<div id=${y}>
-    <p>${x.text}</p>
+    <p onClick="desc(this)">${x.text}</p>
+    <p class="mb-2 d-none fw-semibold">${x.desc}</p>
+    <p class="mb-2 d-none fw-semibold">${x.date}</p>
   </div>`);
   });
+};
+
+let desc = (e) => {
+  selected = e.innerHTML;
+  detailsText.value = selected;
+  modal.style.display = "block";
+  console.log(detailsText.value);
 };
 
 let resetForm = () => {
   textInput.value = "";
   textarea.value = "";
+  date.value = "";
   msg.innerHTML = "";
 };
 
@@ -88,10 +112,14 @@ let deleteTask = (e) => {
 
 let editTask = (e) => {
   let selectedTask = e.parentElement.parentElement;
-
   textInput.value = selectedTask.children[0].innerHTML;
   textarea.value = selectedTask.children[1].innerHTML;
+  date.value = selectedTask.children[2].innerHTML;
   deleteTask(e);
+};
+
+let flagTask = (e) => {
+  e.parentElement.parentElement.classList.toggle("completed");
 };
 
 (() => {
@@ -100,3 +128,8 @@ let editTask = (e) => {
   createTasksNav();
   console.log(data);
 })();
+
+let navupate = (e) => {
+  alert("Updated Succesfully..");
+  modal.style.display = "none";
+};
